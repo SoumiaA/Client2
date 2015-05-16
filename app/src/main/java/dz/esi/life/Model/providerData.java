b1994a3e7ff2bfa3a158fe59ec4ae19a09b666d9
m3ftah <guemar93@gmail.com>
2015-05-16 10:52:24 +0100

    Ajouter providerData

---
+++ app/src/main/java/dz/esi/life/Model/providerData.java
@@ -0,0 +1,16 @@
+package dz.esi.life.Model;
+
+/**
+ * Created by MEFTAH on 15/05/2015.
+ */
+public class ProviderData {
+    public String id;
+    public String email;
+    public String name;
+    public String link;
+    public String picture;
+    public String gender;
+    public String locale;
+    public String accessToken;
+
+}
